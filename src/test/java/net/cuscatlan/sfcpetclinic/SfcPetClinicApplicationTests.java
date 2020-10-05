058daf2f38e8010b32293df972087b1a922e5373
Renato Bonilla <renatio@gmail.com>
2020-10-05 15:05:03 -0600

    Commit Inicial del Proyecto

---
+++ src/test/java/net/cuscatlan/sfcpetclinic/SfcPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.sfcpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfcPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
