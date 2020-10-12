33e6f267bed7799fb7af101731cc020cb2cea1c1
rBonilla <rBonilla@DRML17038.daiglobal.net>
2020-10-12 12:34:03 -0600

    Se ha configurado el controlador y la vista de veterinarios. Closes #15

--- pet-clinic-web/src/main/java/net/cuscatlan/sfcpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/net/cuscatlan/sfcpetclinic/controllers/VetsController.java
@@ -5,15 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import net.cuscatlan.sfcpetclinic.service.VetService;
+
 
 @Controller
 @RequestMapping("/vets")
 public class VetsController {
+	
+	private final VetService vetService;	
+
+	public VetsController(VetService vetService) {
+		this.vetService = vetService;
+	}
 
 	@GetMapping({"","/","/index", "/index.html"})
 	public String findAll(Model model) {
 		model.addAttribute("title", "Vets List");
-
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/list";	
 	}
 
