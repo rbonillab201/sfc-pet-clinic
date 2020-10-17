0887773a87363118f3bcc05f0f6e6ffaca32b2f5
rBonilla <rBonilla@DRML17038.daiglobal.net>
2020-10-17 12:13:50 -0600

    PetType Service and PetType Map Impl created. #29

---
+++ pet-clinic-data/src/main/java/net/cuscatlan/sfcpetclinic/service/PetTypeService.java
@@ -0,0 +1,12 @@
+package net.cuscatlan.sfcpetclinic.service;
+
+import net.cuscatlan.sfcpetclinic.model.PetType;
+
+/**
+ * @author Renato Oswaldo Bonilla (rBonilla) el día Oct 17, 2020
+ *
+ */
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/net/cuscatlan/sfcpetclinic/service/map/PetTypeServiceMap.java
@@ -0,0 +1,45 @@
+package net.cuscatlan.sfcpetclinic.service.map;
+
+import java.util.Set;
+
+import net.cuscatlan.sfcpetclinic.model.PetType;
+import net.cuscatlan.sfcpetclinic.service.PetTypeService;
+
+/**
+ * @author Renato Oswaldo Bonilla (rBonilla) el día Oct 17, 2020
+ *
+ */
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+
+	@Override
+	public Set<PetType> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public PetType findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public PetType save(PetType object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(PetType object) {
+		super.delete(object);
+		
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+		
+	}
+
+
+
+
+}
