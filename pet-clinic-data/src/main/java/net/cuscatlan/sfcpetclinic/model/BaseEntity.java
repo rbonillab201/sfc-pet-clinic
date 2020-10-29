71727f5ae7b9e731c109671d7e5285485c9580e0
rBonilla <rBonilla@DRML17038.daiglobal.net>
2020-10-28 19:54:35 -0600

    Update with JPA annotations BaseEntity. Closes #35

--- pet-clinic-data/src/main/java/net/cuscatlan/sfcpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/net/cuscatlan/sfcpetclinic/model/BaseEntity.java
@@ -2,6 +2,11 @@ package net.cuscatlan.sfcpetclinic.model;
 
 import java.io.Serializable;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Data;
 
 /**
@@ -10,13 +15,15 @@ import lombok.Data;
  */
 
 @Data
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 
